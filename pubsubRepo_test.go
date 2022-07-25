6b25933f325844857fd014caf0360faf38a9c270
singgihdwindaru <singgihlab@gmail.com>
2022-07-26 00:42:32 +0700

    add integration test concurrent

--- pubsubRepo_test.go
+++ pubsubRepo_test.go
@@ -8,21 +8,21 @@ import (
 	"pubsub-cases/src/models"
 	"pubsub-cases/src/repo"
 	"pubsub-cases/src/services"
+	"sync"
 	"testing"
 	"time"
 
 	"cloud.google.com/go/pubsub"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
 	"google.golang.org/api/option"
 )
 
-type pubsubRepoSuite struct {
-	suite.Suite
-	pubsubRepo repo.IPubSubRepo
-}
+// type pubsubRepoSuite struct {
+// 	suite.Suite
+// 	pubsubRepo repo.IPubSubRepo
+// }
 
-func (s *pubsubRepoSuite) SetupSuite()
+// func (s *pubsubRepoSuite) SetupSuite() {}
 func TestIntgPubSub(t *testing.T) {
 	t.Setenv("GOOGLE_APPLICATION_CREDENTIAL", "keyPubSub.json")
 	t.Setenv("PROJECT_ID", "propane-galaxy-168212")
@@ -95,6 +95,19 @@ func TestIntgPubSub(t *testing.T) {
 }
 
 func TestIntgPubSubConcurent(t *testing.T) {
+	t.Setenv("GOOGLE_APPLICATION_CREDENTIAL", "keyPubSub.json")
+	t.Setenv("PROJECT_ID", "propane-galaxy-168212")
+	t.Setenv("TOPIC", "Hello-Concurrent")
+	t.Setenv("SUBSCRIBER", "Hello-Concurrent-sub")
+	t.Setenv("DEAD_SUB_ID", "Hello-Concurrent-dead-sub")
+
+	_pubsubClient, err := pubsub.NewClient(context.Background(), os.Getenv("PROJECT_ID"), option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIAL")))
+	assert.NoError(t, err)
+	defer _pubsubClient.Close()
+
+	repo := repo.NewPubSubRepo(_pubsubClient, os.Getenv("TOPIC"))
+	serv := services.NewPubSubService(repo)
+
 	testTable := []struct {
 		name string
 		sut  func() error
@@ -108,6 +121,43 @@ func TestIntgPubSubConcurent(t *testing.T) {
 		{
 			name: "ConcSubscribe",
 			sut: func() error {
+				wg := &sync.WaitGroup{}
+				subs := []models.SubscriberConfig{
+					{
+						SubscriberId: os.Getenv("DEAD_SUB_ID"),
+						Concurrency:  1,
+						Handler: func(ctx context.Context, message *pubsub.Message) {
+							fmt.Printf("%s : Ack-ing %s\n", os.Getenv("DEAD_SUB_ID"), string(message.Data))
+							message.Ack()
+						},
+					},
+					{
+						SubscriberId: os.Getenv("SUBSCRIBER"),
+						Concurrency:  1,
+						Handler: func(ctx context.Context, message *pubsub.Message) {
+							param := models.PubSubModels{}
+							err := json.Unmarshal(message.Data, &param)
+							if err != nil {
+								fmt.Printf("%s : Fail Consuming %s\n", os.Getenv("SUBSCRIBER"), string(message.Data))
+								message.Nack()
+								return
+							}
+							fmt.Printf("%s : Success Consuming %s\n", os.Getenv("SUBSCRIBER"), string(message.Data))
+							message.Ack()
+						},
+					},
+				}
+				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+				defer cancel()
+				for _, s := range subs {
+					wg.Add(1)
+					go func(swg *sync.WaitGroup, cfg models.SubscriberConfig) error {
+						defer swg.Done()
+						err = serv.PullMsg(ctx, cfg)
+						return err
+					}(wg, s)
+				}
+				wg.Wait()
 				return nil
 			},
 		},
@@ -124,7 +174,8 @@ func TestIntgPublishMsg(t *testing.T) {
 	assert := assert.New(t)
 	t.Setenv("GOOGLE_APPLICATION_CREDENTIAL", "keyPubSub.json")
 	t.Setenv("PROJECT_ID", "propane-galaxy-168212")
-	t.Setenv("TOPIC", "hello-pubsub")
+	// t.Setenv("TOPIC", "hello-pubsub")
+	t.Setenv("TOPIC", "Hello-Concurrent")
 	_pubsubClient, err := pubsub.NewClient(context.Background(), os.Getenv("PROJECT_ID"), option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIAL")))
 	assert.NoError(err)
 	defer _pubsubClient.Close()
